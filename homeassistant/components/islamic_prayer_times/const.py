daf90df30968a5d282e05765e379e56de763314c
Zuhaib Siddique <zsiddique@gmail.com>
2023-08-20 02:39:52 +0000

    adding LAT_ADJ_METHODS, MIDNIGHT_MODES, SCHOOLS, and CALC_METHODS directly to the component

--- homeassistant/components/islamic_prayer_times/const.py
+++ homeassistant/components/islamic_prayer_times/const.py
@@ -1,23 +1,38 @@
 """Constants for the Islamic Prayer component."""
 from typing import Final
 
-from prayer_times_calculator import PrayerTimesCalculator
-
 DOMAIN: Final = "islamic_prayer_times"
 NAME: Final = "Islamic Prayer Times"
 
 CONF_CALC_METHOD: Final = "calculation_method"
-CALC_METHODS: list[str] = list(PrayerTimesCalculator.CALCULATION_METHODS)
+CALC_METHODS: list[str] = [
+    "jafari",
+    "karachi",
+    "isna",
+    "mwl",
+    "makkah",
+    "egypt",
+    "tehran",
+    "gulf",
+    "kuwait",
+    "qatar",
+    "singapore",
+    "france",
+    "turkey",
+    "russia",
+    "moonsighting",
+    "custom",
+]
 DEFAULT_CALC_METHOD: Final = "isna"
 
 CONF_LAT_ADJ_METHOD: Final = "latitude_adjustment_method"
-LAT_ADJ_METHODS: list[str] = list(PrayerTimesCalculator.LAT_ADJ_METHODS)
+LAT_ADJ_METHODS: list[str] = ["middle of the night", "one seventh", "angle based"]
 DEFAULT_LAT_ADJ_METHOD: Final = "middle of the night"
 
 CONF_MIDNIGHT_MODE: Final = "midnight_mode"
-MIDNIGHT_MODES: list[str] = list(PrayerTimesCalculator.MIDNIGHT_MODES)
+MIDNIGHT_MODES: list[str] = ["standard", "jafari"]
 DEFAULT_MIDNIGHT_MODE: Final = "standard"
 
 CONF_SCHOOL: Final = "school"
-SCHOOLS: list[str] = list(PrayerTimesCalculator.SCHOOLS)
+SCHOOLS: list[str] = ["shafi", "hanafi"]
 DEFAULT_SCHOOL: Final = "shafi"
